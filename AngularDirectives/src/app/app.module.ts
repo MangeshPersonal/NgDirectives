76e489195e8bbee453cdf36085c6f8fc89dfacc2
MangeshG <Mangesh.g@intelgain.com>
2017-11-22 15:52:26 +0530

    new Changes

--- AngularDirectives/src/app/app.module.ts
+++ AngularDirectives/src/app/app.module.ts
@@ -1,15 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-import { NewAppcomponentComponent } from './new-appcomponent/new-appcomponent.component';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NewAppcomponentComponent
+    AppComponent
+   
   ],
   imports: [
     BrowserModule
